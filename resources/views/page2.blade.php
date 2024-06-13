ad68a4e883923b9dffe4a9aa4bdbd667e262612e
Makhsat Saniyaz <makhsatsaniyaz@gmail.com>
2024-06-13 12:53:48 +0700

    update

--- resources/views/page2.blade.php
+++ resources/views/page2.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dynamic Brick Wall</title>
+    <title>New Tasom</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
@@ -28,8 +28,12 @@
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Contact</a>
-                </li><li class="nav-item">
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" href="{{ route('login') }}">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">Register</a>
                 </li>
             </ul>
         </div>
