873b5c160fe02e2c5f9448af7b5a217fde157873
ertce37 <rul@DESKTOP-E1RTT7D>
2024-07-02 18:11:38 -0500

    updated donation amount

--- resources/views/page2.blade.php
+++ resources/views/page2.blade.php
@@ -859,11 +859,11 @@
         }
 
         // Example usage: filling the wall with 330 bricks
-        fillBrickWall(401);
+        fillBrickWall(403);
     </script>
  <script>
          // Define the variable for the number of sold bricks
-         var numberOfSoldBricks = 401; // Update this variable as needed
+         var numberOfSoldBricks = 403; // Update this variable as needed
 
          // Function to update the brick count in the HTML
          function updateBrickCount() {
