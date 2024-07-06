1d3aa07b29c36f9b0370b67c9c76747f7ddb6450
ertce37 <rul@DESKTOP-E1RTT7D>
2024-07-05 23:14:56 -0500

    admin panel added

--- resources/views/page2.blade.php
+++ resources/views/page2.blade.php
@@ -859,11 +859,11 @@
         }
 
         // Example usage: filling the wall with 330 bricks
-        fillBrickWall(525);
+        fillBrickWall(522);
     </script>
  <script>
          // Define the variable for the number of sold bricks
-         var numberOfSoldBricks = 525; // Update this variable as needed
+         var numberOfSoldBricks = 522; // Update this variable as needed
 
          // Function to update the brick count in the HTML
          function updateBrickCount() {
